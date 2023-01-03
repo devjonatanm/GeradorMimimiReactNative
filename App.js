20df0fcf4d9c01ed6ff4321666a1e5cc41b955c7
Jonatan Marques <102691032+devjonatanmarquess@users.noreply.github.com>
2023-01-02 22:31:27 -0300

    segunda modificação

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 export default class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {texto1:'texto1',texto2:'texto2'};
+    this.state = { texto1: 'texto1', texto2: 'texto2' };
     this.escrever = this.escrever.bind(this);
   }
 
-  mudarVogais(t){
+  mudarVogais(t) {
     let novo = t.toLowerCase();
     novo = novo.replace(/(a|e|i|o|u)/g, 'i');
     novo = novo.replace(/(á|à|ã|â)/g, 'i');
@@ -20,7 +20,7 @@ export default class App extends React.Component {
     return novo;
   }
 
-  escrever(t){
+  escrever(t) {
     let s = this.state;
     s.texto1 = t;
     s.texto2 = this.mudarVogais(t);
@@ -39,9 +39,9 @@ export default class App extends React.Component {
         </View>
 
         <View style={styles.area}>
-            <Text style={[styles.texto,styles.texto1]}>{this.state.texto1.toUpperCase()}</Text>
-            <Image style={styles.guri} source={require('./images/mimimi.jpg')}></Image>
-            <Text style={[styles.texto,styles.texto2]}>{this.state.texto2.toUpperCase()}</Text>
+          <Text style={[styles.texto, styles.texto1]}>{this.state.texto1.toUpperCase()}</Text>
+          <Image style={styles.guri} source={require('./images/mimimi.jpg')}></Image>
+          <Text style={[styles.texto, styles.texto2]}>{this.state.texto2.toUpperCase()}</Text>
         </View>
       </View>
     );
@@ -51,51 +51,51 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    paddingTop:30,
+    paddingTop: 30,
     backgroundColor: '#999999',
     alignItems: 'center',
     flexDirection: 'column',
   },
-  titulo:{
-    fontSize:30,
-    color:'#FFFFFF'
+  titulo: {
+    fontSize: 30,
+    color: '#FFFFFF'
   },
-  inputArea:{
-    alignSelf:'stretch'
+  inputArea: {
+    alignSelf: 'stretch'
   },
-  input:{
-    borderWidth:1,
-    borderColor:"#EEEEEE",
-    color:"#000000",
-    height:40,
-    margin:20,
-    padding:10
+  input: {
+    borderWidth: 1,
+    borderColor: "#EEEEEE",
+    color: "#000000",
+    height: 40,
+    margin: 20,
+    padding: 10
   },
-  area:{
-    width:300,
-    height:300,
-    marginTop:10
+  area: {
+    width: 300,
+    height: 300,
+    marginTop: 10
   },
-  guri:{
-    width:300,
-    height:300,
-    marginTop:-70,
-    zIndex:0
+  guri: {
+    width: 300,
+    height: 300,
+    marginTop: -70,
+    zIndex: 0
   },
-  texto:{
-    fontSize:25,
-    color:"#FFFFFF",
-    padding:10,
-    backgroundColor:"transparent",
-    fontWeight:'bold',
-    textAlign:'center',
-    height:70
+  texto: {
+    fontSize: 25,
+    color: "#FFFFFF",
+    padding: 10,
+    backgroundColor: "transparent",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    height: 70
   },
-  texto1:{
-    zIndex:1
+  texto1: {
+    zIndex: 1
   },
-  texto2:{
-    zIndex:1,
-    marginTop:-70
+  texto2: {
+    zIndex: 1,
+    marginTop: -70
   }
 });
